893584382187d4d948367351533620bb77529e6e
Marcelo Pilatti <marcelo.silva979@gmail.com>
2025-03-18 00:42:38 +0000

    Deploy

--- chunk-KXNYEN7R.js
+++
@@ -1,1 +0,0 @@
-import{Kb as r,ab as n,ub as o,vb as i}from"./chunk-I44CEVIG.js";var s=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["pgm-library"]],decls:2,vars:0,template:function(t,p){t&1&&(o(0,"p"),r(1,"library works!"),i())},encapsulation:2})}}return e})();export{s as LibraryComponent};
