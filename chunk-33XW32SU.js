1afa12d0446a825ad4355abd587aff68e39bde2a
Marcelo Pilatti <marcelo.silva979@gmail.com>
2025-03-26 21:09:30 +0000

    Deploy

--- chunk-33XW32SU.js
+++
@@ -1,1 +0,0 @@
-import{Lb as c,ab as i,vb as n,wb as o}from"./chunk-KWJVQ5UB.js";var s=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=i({type:e,selectors:[["pgm-scientific-outreach"]],decls:2,vars:0,template:function(t,a){t&1&&(n(0,"p"),c(1,"scientific-outreach works!"),o())},encapsulation:2})}}return e})();export{s as ScientificOutreachComponent};
