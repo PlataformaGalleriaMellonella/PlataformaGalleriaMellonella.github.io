f206562e097d33ae77cd870c837b72d78edac6a4
Marcelo Pilatti <marcelo.silva979@gmail.com>
2025-05-08 22:16:23 +0000

    Deploy

---
+++ chunk-I65356U4.js
@@ -0,0 +1,1 @@
+import{Gb as i,Ya as o,rb as n,sb as s}from"./chunk-UOQ32YYF.js";var p=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=o({type:e,selectors:[["pgm-profile-messages"]],decls:2,vars:0,template:function(t,r){t&1&&(n(0,"p"),i(1,"community-messages works!"),s())},encapsulation:2})}}return e})();export{p as ProfileMessagesComponent};
